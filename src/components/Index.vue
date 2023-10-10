0c3697eb40c0381a2ed73375653312af15c4b0dd
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-10-10 17:50:28 +0200

    feat: sass installed

---
+++ src/components/Index.vue
@@ -0,0 +1,45 @@
+<script>
+import axios from "axios";
+
+export default {
+  name: "Index",
+
+  data() {
+    return {};
+  },
+  created() {
+    this.getPizzas();
+  },
+  methods: {
+    getPizzas() {
+      axios
+        .get("https://localhost:7249/api/Pizzas/GetPizzas")
+        .then((response) => {
+          if (response.data.success) {
+            console.log(response.data);
+          } else {
+            console.log("We did not find any pizza");
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="__red">
+    <h1 class="__blue">This is my front end</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.__red {
+  background-color: red;
+  .__blue {
+    color: blue;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.scss";
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(BootstrapVue3);
+app.mount("#app");
